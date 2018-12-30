b7945beb8508b759d5e1a341b938186fb5e63f20
Joppe Geluykens <joppe.geluykens@student.kuleuven.be>
2018-12-30 01:04:43 +0100

    write model details to page instead of popup tfjs-vis surface

--- gatys-2015/index.js
+++ gatys-2015/index.js
@@ -17,9 +17,59 @@ document.addEventListener('DOMContentLoaded', function() {
   initModel();
 });
 
+function formatShape(shape) {
+  const oShape = shape.slice();
+  if (oShape.length === 0) {
+    return 'Scalar';
+  }
+  if (oShape[0] === null) {
+    oShape[0] = 'batch';
+  }
+  return `[${oShape.join(',')}]`;
+}
+
+/*
+ * Gets summary information/metadata about a layer.
+ */
+function getLayerSummary(layer) {
+  let outputShape;
+  if (Array.isArray(layer.outputShape[0])) {
+    const shapes = (layer.outputShape).map(s => formatShape(s));
+    outputShape = `[${shapes.join(', ')}]`;
+  } else {
+    outputShape = formatShape(layer.outputShape);
+  }
+
+  return {
+    name: layer.name,
+    trainable: layer.trainable,
+    parameters: layer.countParams(),
+    outputShape,
+  };
+}
+
 async function showModel() {
-  const surface = { name: 'Model Summary', tab: 'Model' };
-  tfvis.show.modelSummary(surface, model);
+  const summary = {
+    layers: model.layers.map(getLayerSummary),
+  };
+
+  const headers = [
+    'Layer Name',
+    'Output Shape',
+    '# Of Params',
+    'Trainable',
+  ];
+
+  const values = summary.layers.map(
+      l =>
+          [l.name,
+           l.outputShape,
+           l.parameters,
+           l.trainable,
+  ]);
+  const summaryContainer = document.getElementById('summary-canvas');
+  tfvis.render.table({headers, values}, summaryContainer);
 }
 document.querySelector('#show-model').addEventListener('click', showModel);
+
 
