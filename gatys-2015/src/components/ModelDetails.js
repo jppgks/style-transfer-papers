2f00c278136b472e5215b33348f6f47002fafa46
Joppe Geluykens <joppe.geluykens@student.kuleuven.be>
2019-01-01 21:34:26 +0100

    log model prediction of random image + add Ant UI framework

---
+++ gatys-2015/src/components/ModelDetails.js
@@ -0,0 +1,129 @@
+import React, {Component} from 'react';
+import * as tfvis from "@tensorflow/tfjs-vis";
+
+import {Row, Button, Table} from "antd";
+
+function formatShape(shape) {
+  const oShape = shape.slice();
+  if (oShape.length === 0) {
+    return 'Scalar';
+  }
+  if (oShape[0] === null) {
+    oShape[0] = 'batch';
+  }
+  return `[${oShape.join(',')}]`;
+}
+
+/*
+ * Gets summary information/metadata about a layer.
+ */
+function getLayerSummary(layer) {
+  let outputShape;
+  if (Array.isArray(layer.outputShape[0])) {
+    const shapes = (layer.outputShape).map(s => formatShape(s));
+    outputShape = `[${shapes.join(', ')}]`;
+  } else {
+    outputShape = formatShape(layer.outputShape);
+  }
+
+  return {
+    name: layer.name,
+    trainable: layer.trainable,
+    parameters: layer.countParams(),
+    outputShape,
+  };
+}
+
+
+class ModelDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.showModel = this.showModel.bind(this);
+    this.getDataSource = this.getDataSource.bind(this);
+    this.toggleTable = this.toggleTable.bind(this);
+
+    this.state = {
+      columns: this.getColumns(),
+      dataSource: this.getDataSource(),
+      showTable: false
+    }
+  }
+
+  getColumns() {
+    return [
+      {
+        title: 'Index', dataIndex: 'key', key: 'key',
+        width: 100,
+        sorter: (a, b) => a.key - b.key,
+      },
+      {
+        title: 'Layer Name', dataIndex: 'layerName', key: 'layerName',
+        width: 200,
+        filters: [{text: 'Trainable', value: 'Trainable'}],
+        onFilter: (value, record) => value === 'Trainable' ? record.trainable : true,
+      },
+      {
+        title: 'Output Shape', dataIndex: 'outputShape', key: 'outputShape',
+        width: 300,
+      },
+      {
+        title: '# Of Params', dataIndex: 'numParams', key: 'numParams',
+        sorter: (a, b) => a.numParams - b.numParams,
+      }
+    ];
+  }
+
+  getDataSource() {
+    const summary = {
+      layers: this.props.model.layers.map(getLayerSummary),
+    };
+
+    let rowId = 1;
+
+    return summary.layers.map(
+      l => {
+        return {
+          'key': rowId++,
+          'layerName': l.name,
+          'outputShape': l.outputShape,
+          'numParams': l.parameters,
+          'trainable': l.trainable,
+        }
+      });
+  }
+
+  toggleTable() {
+    this.setState({showTable: !this.state.showTable});
+  }
+
+  showModel() {
+
+
+    const summaryContainer = document.getElementById('summary-canvas');
+    tfvis.render.table({headers, values}, summaryContainer);
+  }
+
+  render() {
+    return (
+      <div>
+        <Row style={{marginBottom: 10}}>
+          <h3>Model Details</h3>
+          <Button onClick={this.toggleTable}>Model summary</Button>
+        </Row>
+        <Row>
+          {/*<br/>*/}
+          {this.state.showTable &&
+          <Table columns={this.state.columns} dataSource={this.state.dataSource}
+                 size={'small'}
+                 pagination={{pageSize: 50, position: 'none'}} scroll={{y: 250}}
+                 footer={() => <span><a href='https://github.com/DavidCai1993/vgg19-tensorflowjs-model'>VGG model</a> exported for TF.js by David Cai.</span>}
+          />
+          }
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default ModelDetails;
--- gatys-2015/src/index.js
+++ gatys-2015/src/index.js
@@ -1,126 +1,147 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import './css/tfjs-examples.css';
-import './css/index.css';
-
 import * as tf from '@tensorflow/tfjs';
 import * as tfvis from '@tensorflow/tfjs-vis';
-const path = require('path');
+
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import ModelDetails from "./components/ModelDetails";
+
+import {Col, Row, Spin, message, Button, Skeleton, Card} from "antd";
+import "antd/dist/antd.css";
 
 window.tf = tf;
 window.tfvis = tfvis;
 
-window.model;
+class RootComp extends Component {
+  // async function initModel() {
+  initModel() {
+    this.setState({modelLoading: true});
+    const successMessage = () => {
+      message.success('Model loaded successfully.');
+    };
 
-async function initModel() {
-  // Replace with host serving VGG model over HTTP, then remove model/ from project dir
-  const model_host = '127.0.0.1:8080'; // 'raw.githubusercontent.com/jppgks/vgg19-tensorflowjs-model/master/model';
-  window.model = await tf.loadModel(`http://${model_host}/model.json`);
-  //window.model.summary();
-}
+    // Replace with host serving VGG model over HTTP, then remove model/ from project dir
+    const model_host = '127.0.0.1:8080'; // 'raw.githubusercontent.com/jppgks/vgg19-tensorflowjs-model/master/model';
 
-function formatShape(shape) {
-  const oShape = shape.slice();
-  if (oShape.length === 0) {
-    return 'Scalar';
-  }
-  if (oShape[0] === null) {
-    oShape[0] = 'batch';
-  }
-  return `[${oShape.join(',')}]`;
-}
-
-/*
- * Gets summary information/metadata about a layer.
- */
-function getLayerSummary(layer) {
-  let outputShape;
-  if (Array.isArray(layer.outputShape[0])) {
-    const shapes = (layer.outputShape).map(s => formatShape(s));
-    outputShape = `[${shapes.join(', ')}]`;
-  } else {
-    outputShape = formatShape(layer.outputShape);
+    tf.loadModel(`http://${model_host}/model.json`)
+      .then((model) => {
+        this.model = model;
+        this.setState({modelLoaded: true, modelLoading: false});
+        // Show toast.
+        successMessage();
+        console.debug("Model loaded.");
+      });
   }
 
-  return {
-    name: layer.name,
-    trainable: layer.trainable,
-    parameters: layer.countParams(),
-    outputShape,
-  };
-}
-
-async function showModel() {
-  const summary = {
-    layers: model.layers.map(getLayerSummary),
-  };
-
-  const headers = [
-    'Layer Name',
-    'Output Shape',
-    '# Of Params',
-    'Trainable',
-  ];
-
-  const values = summary.layers.map(
-      l =>
-          [l.name,
-           l.outputShape,
-           l.parameters,
-           l.trainable,
-  ]);
-  const summaryContainer = document.getElementById('summary-canvas');
-  tfvis.render.table({headers, values}, summaryContainer);
-}
-
-class RootComp extends React.Component {
   constructor(props) {
     super(props);
-    initModel();
+    this.state = {
+      modelLoading: false,
+      modelLoaded: false
+    };
+    this.initModel = this.initModel.bind(this);
+    this.predictRandomImage = this.predictRandomImage.bind(this);
   }
 
-  componentDidMount() {
-    document.querySelector('#show-model').addEventListener('click', showModel);
+  generateRandomInputImage() {
+    let imageData = new ImageData(224, 224);
+
+    // Fill image with 'color wheel'-like data
+    for (let i = 0; i < imageData.data.length; i += 4) {
+      // Percentage in the x direction, times 255
+      let x = (i % (224*4)) / (224*4) * 255;
+      // Percentage in the y direction, times 255
+      let y = Math.ceil(i / (224*4)) / 100 * 255;
+
+      // Modify pixel data
+      imageData.data[i] = x; // R value
+      imageData.data[i + 1] = y; // G value
+      imageData.data[i + 2] = 255 - x; // B value
+      imageData.data[i + 3] = 255; // A value
+    }
+
+    const canvas = document.getElementById('random-input-img');
+    const ctx = canvas.getContext('2d');
+    ctx.putImageData(imageData, 0, 0);
+
+    // Return 4D Tensor (batch, )
+    return tf.expandDims(tf.fromPixels(imageData));
+  }
+
+  predictRandomImage() {
+    // Check that calling model works, perhaps also try to get layer output now
+    let randomImage = this.generateRandomInputImage();
+    randomImage.print(true); // verbose print
+    this.model.predict(randomImage, {batchSize: 1}).print();
+    randomImage.dispose();
+  }
+
+  loadButtonText() {
+    let text = "loadButtonText";
+    if (!this.state.modelLoaded) {
+      if (this.state.modelLoading) {
+        text = "Loading model";
+      } else {
+        text = "Load model";
+      }
+    } else {
+      text = "Model loaded"
+    }
+    return text;
   }
 
   render() {
-      return (<div>
-			 <div className="tfjs-example-container">
-				 <section className='title-area'>
-				   <h1>TensorFlow.js: Style Transfer</h1>
-				   <p className='subtitle'>TK subtitle
-				   </p>
-				 </section>
-				 
-				 <section>
-				   <p className='section-head'>Description</p>
-				   <p>
-					 TK
-				   </p>
-				 </section>
-				 
-				 <section>
-				   <p className='section-head'>Training Parameters</p>
-				   
-				   <button id="show-model">Describe Model</button>
-				 </section>
-				 
-				 <section>
-				   <p className='section-head'>Model Details</p>
-				   <div id="stats">
-					 <div className="canvases">
-					   <label id="summary-label"></label>
-					   <div id="summary-canvas"></div>
-					 </div>
-				   </div>
-				 </section>
-			</div>
-        </div>)
+    return (
+      <div>
+        <Row>
+          <Col span={2}></Col>
+          <Col span={20}>
+            <div className="tfjs-example-container">
+              <h1>TensorFlow.js: Style Transfer</h1>
+              <h2>TK subtitle</h2>
+            </div>
+          </Col>
+          <Col span={2}></Col>
+        </Row>
+        <Row>
+          <Col span={2}></Col>
+          <Col span={20}>
+            <h3>Description</h3>
+            <p>TK</p>
+          </Col>
+          <Col span={2}></Col>
+        </Row>
+        <Row>
+          <Col span={2}></Col>
+          <Col span={20}>
+            <p>
+              Click the button to load the VGG-19 model into memory,
+              so we can use it to perform style transfer.
+            </p>
+            <Button type={"primary"} onClick={this.initModel}
+                    loading={this.state.modelLoading} disabled={this.state.modelLoaded}>
+              {this.loadButtonText()}
+            </Button>
+            {this.state.modelLoading &&
+              <Skeleton loading={!this.state.modelLoaded} active title paragraph={{rows: 1}} />
+            }
+            {this.state.modelLoaded && (
+              <div>
+                <ModelDetails model={this.model}/>
+                <Button onClick={this.predictRandomImage}>Log prediction of random image</Button>
+                <Card style={{ width: 224 }}
+                      cover={<canvas width={224} height={224} id="random-input-img"/>}>
+                  Random input image
+                </Card>
+              </div>
+            )}
+          </Col>
+          <Col span={2}></Col>
+        </Row>
+      </div>)
   }
 }
 
 let App = document.getElementById("app");
 
-ReactDOM.render(<RootComp />, App);
+ReactDOM.render(<RootComp/>, App);
 
